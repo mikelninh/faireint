e6335665a54251748181a95c44335daa807c44d1
Mikel <34214125+mikelninh@users.noreply.github.com>
2026-08-23 09:12:16 +0200

    Route education and reality labs

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,28 @@ import App from './App.tsx'
 import DiwWealthLab from './DiwWealthLab.tsx'
 import FairEintV2 from './FairEintV2.tsx'
 import FairEintV5 from './FairEintV5.tsx'
+import RealityLab from './RealityLab.tsx'
+import WorkshopLab from './WorkshopLab.tsx'
 
 const params = new URLSearchParams(window.location.search)
 const showLegacy = params.get('legacy') === '1'
 const showV2 = params.get('v2') === '1'
 const showDiwLab = params.get('diw') === '1'
+const showRealityLab = params.get('reality') === '1'
+const showWorkshopLab = params.get('workshop') === '1'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {showLegacy ? <App /> : showV2 ? <FairEintV2 /> : showDiwLab ? <DiwWealthLab /> : <FairEintV5 />}
+    {showLegacy
+      ? <App />
+      : showV2
+        ? <FairEintV2 />
+        : showDiwLab
+          ? <DiwWealthLab />
+          : showRealityLab
+            ? <RealityLab />
+            : showWorkshopLab
+              ? <WorkshopLab />
+              : <FairEintV5 />}
   </StrictMode>,
 )
